e26964d92c8541e3afba21df1a8bfd2d9790ad48
Mostafizur rahman <mrshimul000@gmail.com>
2020-02-22 11:21:58 +0600

    =Added the dynamic health color

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 new Vue({
     el: '#app',
     data: {
-        playerHealth: 100,
-        monsterHealth: 100,
-        gameIsRunning: false
+        playerHealth:       100,
+        monsterHealth:      100,
+        gameIsRunning:      false,
+        playerHealthColor:  '#15935e',
+        monsterHealthColor: '#15935e'
     },
     methods: {
         startGame: function(){
-            this.gameIsRunning = true
-            this.playerHealth = 100
-            this.monsterHealth = 100
+            this.gameIsRunning      = true
+            this.playerHealth       = 100
+            this.monsterHealth      = 100
+            this.playerHealthColor  = '#15935e'
+            this.monsterHealthColor = '#15935e'
         },
         attack: function(){
             //attack player to the monster and damage the health of the monster
             this.playerAttacks(3, 10)
-
+            // if(this.checkWin()){
+            //     return 
+            // }
+            this.checkWin()
             //attack monster to the player and damage the health of the player
             this.monsterAttacks(3, 12)
+            this.checkWin()
         },
         specialAttack: function(){
             //attack monster to the player and damage the health of the player
             this.playerAttacks(10, 15)
+            this.checkWin()
         },
         heal: function(){
             
@@ -31,14 +40,18 @@ new Vue({
         monsterAttacks: function(min, max){
             // damage player health
             this.playerHealth -= this.calculateDamage(min, max)
+            // change the health color
+            this._playerHealthColor()
             // if player dead? end game else continue
-            this.checkWin()
+            // this.checkWin()
         },
         playerAttacks: function(min, max){
             // damage the monster health
             this.monsterHealth -= this.calculateDamage(min, max)
+            // change the health color
+            this._monsterHealthColor()
             // if monster dead? end game else continue
-            this.checkWin()
+            // this.checkWin()
         },
         calculateDamage: function(min, max){
             return Math.max(Math.floor((Math.random() * max) + 1), min)
@@ -60,6 +73,24 @@ new Vue({
                 return true
             }
             return false
+        },
+        _playerHealthColor: function(){
+            if (this.playerHealth < 90 && this.playerHealth >= 50) {
+                this.playerHealthColor = '#24a46e';
+            } else if (this.playerHealth < 50 && this.playerHealth >= 25) {
+                this.playerHealthColor = '#38bd85';
+            } else if (this.playerHealth < 25) {
+                this.playerHealthColor = '#d94343';
+            }
+        },
+        _monsterHealthColor: function(){
+            if (this.monsterHealth < 90 && this.monsterHealth >= 50) {
+                this.monsterHealthColor = '#24a46e';
+            } else if (this.monsterHealth < 50 && this.monsterHealth >= 25) {
+                this.monsterHealthColor = '#38bd85';
+            } else if (this.monsterHealth < 25) {
+                this.monsterHealthColor = '#d94343';
+            }
         }
     }
 });
